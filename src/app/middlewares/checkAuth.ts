75af991559f6581201b82d1a3b44b9594b018d7e
asif100a <asifsheikhrrr@gmail.com>
2026-05-18 23:00:42 +0600

    Implement authentication middleware and add not found handler; create auth module files

--- src/app/middlewares/checkAuth.ts
+++ src/app/middlewares/checkAuth.ts
@@ -3,7 +3,12 @@ import type { NextFunction, Request, Response } from "express";
 export const checkAuth = (...authRoles: string[]) => async(req: Request, res: Response, next: NextFunction) => {
     try {
         const accessToken = req.cookies();
+        console.log('Access token from the checkAuth: ', accessToken)
+
+        // Call the next function
+        next()
     } catch (error) {
-        
+        console.log('❌ Auth check error: ', error)
+        next(error)
     }
 }
