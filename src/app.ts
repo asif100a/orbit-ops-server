dff94dc9f7e60ceb58d52cc968b5ff0e69a13147
asif100a <asifsheikhrrr@gmail.com>
2026-05-21 10:06:56 +0600

    Refactor app setup: organize middleware usage and add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,46 @@
-import express, { type Request, type Response } from 'express'
-import expressSession from 'express-session'
-import { envConfig } from './app/config/env'
-import cors from 'cors'
-import router from './app/routes';
-import notFound from './app/middlewares/notFound';
+import express, { type Request, type Response } from "express";
+import expressSession from "express-session";
+import { envConfig } from "./app/config/env";
+import cors from "cors";
+import router from "./app/routes";
+import notFound from "./app/middlewares/notFound";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 
-const app = express()
+const app = express();
 
-app.use(expressSession({
+// Security & Cors
+app.use(cors());
+
+// Body parser
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+// Session
+app.use(
+  expressSession({
     secret: envConfig.EXPRESS_SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
-}))
-app.use(express.json())
-app.use(cors())
+    saveUninitialized: false,
+    cookie: {
+      secure: envConfig.NODE_ENV === "production",
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      sameSite: envConfig.NODE_ENV === "production" ? "none" : "lax",
+    },
+  }),
+);
 
-app.get('/', (req: Request, res: Response) => {
-    res.status(200).json({
-        message: 'Welcome to the OrbitOps server'
-    })
-})
-
-app.use('/api/v1', router)
+// Routes
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Welcome to the OrbitOps server",
+  });
+});
+app.use("/api/v1", router);
 
 // Not found route
-app.use(notFound)
+app.use(notFound);
+// Global Error handler
+app.use(globalErrorHandler);
 
 export default app;
